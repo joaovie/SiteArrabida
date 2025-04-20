dac8d6295a9224811f642f6ca8298d9b5dcf8681
joao.vieira <50412170+joaovie@users.noreply.github.com>
2025-04-20 19:29:17 +0100

    updated image format

--- js/tour_images.js
+++ js/tour_images.js
@@ -1,22 +1,22 @@
 // This file contains the images for the tours.
 const tourImages = {
     'rota_castelos': [
-        'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510576.jpg',
-        'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510556.jpg',
-        'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510581.jpg',
-        'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510587.jpg'
+        'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510576.webp',
+        'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510556.webp',
+        'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510581.webp',
+        'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510587.webp'
     ],
     'maravilhas_arrabida': [
-        'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_1.jpeg',
-        'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/P1510646.jpg',
-        'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_2.jpeg',
-        'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_4.jpeg',
+        'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_1.webp',
+        'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/P1510646.webp',
+        'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_2.webp',
+        'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_4.webp',
     ],
     'terras_do_cabo_espichel': [
-        'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/IMG_2.jpeg',
-        'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/IMG_3.jpeg',
-        'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/P1510701.jpeg',
-        'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/IMG_1249.png',
+        'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/IMG_2.webp',
+        'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/IMG_3.webp',
+        'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/P1510701.webp',
+        'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/IMG_1249.webp',
     ]
 };
 
@@ -32,14 +32,14 @@ if (typeof module !== 'undefined' && module.exports) {
 
 // Hero carousel images
 const heroImages = [
-    'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/IMG_1230 2.jpeg',
-    'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_1.jpeg',
-    'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510576.jpg',
-    'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510581.jpg',
-    'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510587.jpg',
-    'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/IMG_2.jpeg',
-    'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_3.jpeg',
-    'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_4.jpeg',
+    'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/IMG_1230 2.webp',
+    'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_1.webp',
+    'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510576.webp',
+    'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510581.webp',
+    'Tours/Arrabida/rota-dos-castelos/Img_rota_castelos/P1510587.webp',
+    'Tours/Arrabida/terras-do-cabo-espichel/Img_terras_do_cabo_espichel/IMG_2.webp',
+    'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_3.webp',
+    'Tours/Arrabida/maravilhas-arrabida/Img_maravilhas_arrabida/IMG_4.webp',
 ];
 
 // If you want to make this data available to other JavaScript files:
